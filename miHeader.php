158373538de406fd34b1173eaf1cdde141ac8483
SergiodelValleS <sergiodvsva@gmail.com>
2025-03-31 21:27:56 +0200

    adawd

---
+++ miHeader.php
@@ -0,0 +1,9 @@
+<div class="contenedorHeader">
+    <div class="logoDiv w-25 centrar">a</div>
+    <div class="row w-75 m-0 centrar">
+        <div class="col centrar" title="Libros publicados"><p class="pHeader">Libros</div>
+        <div class="col centrar"><p class="pHeader">b</div>
+        <div class="col centrar"><p class="pHeader">c</div>
+        <div class="col centrar"><p class="pHeader">d</div>
+</div>
+</div>
